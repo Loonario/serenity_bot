c197c795d9aa95bd57082dc86363b3ee8fe54eb0
“Alexander <alexloonatik@gmail.com>
2023-08-01 15:31:23 +0300

    find user function fixes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// const express = require('express')
 require('dotenv').config()
 if (process.env.NODE_ENV == 'development') {
   require('dotenv').config({ path: '.env.dev' })
@@ -17,49 +16,12 @@ const { userCommands } = require('./user/variables')
 const { adminCommands } = require('./admin/variables')
 const remove_kb = Markup.removeKeyboard()
 const superAdmin = process.env.SUPERADMIN_CHATID
-//const SceneGenerator = require('./Scenes')
-// const curScene = new SceneGenerator()
-// const nameScene = curScene.NameScene()
-// const app = express()
-// Airtable.configure({
-//   endpointUrl: 'https://api.airtable.com',
-//   apiKey: process.env.AIRTABLE_API_KEY,
-// })
+
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
   process.env.AIRTABLE_BASE_ID,
 )
 const tableUsers = process.env.AIRTABLE_USERS_TABLE_ID
 let currentUser = {}
-
-//Admin stage
-const adminStage = new Stage([setAdminWizard])
-setAdminWizard.enter(async ctx => {
-  await ctx.reply('ID?')
-})
-
-adminStage.hears('Вийти', async ctx => {
-  await ctx.reply('Оберіть послугу в Меню', remove_kb)
-  return ctx.scene.leave()
-})
-
-// User services stage
-const userStage = new Stage([gsWizard])
-gsWizard.enter(async ctx => {
-  await ctx.reply('Будь-ласка, вкажіть ПІБ людини')
-})
-
-userStage.hears('Вийти', async ctx => {
-  await ctx.reply('Оберіть послугу в Меню', remove_kb)
-  return ctx.scene.leave()
-})
-
-userStage.hears('Підтвердити', async ctx => {
-  await ctx.reply(
-    'Дякую, Ваші дані прийняті. Скоро ми надішлемо фото для звірки',
-    remove_kb,
-  )
-  return ctx.scene.leave()
-})
 
 // Bot initialization
 let bot
@@ -74,17 +36,66 @@ if (process.env.NODE_ENV == 'development') {
   //bot.startWebhook('/', null, 8443)
 }
 //bot.use(Telegraf.log())
-bot.use(session(), userStage.middleware(), adminStage.middleware())
+
+//Admin stage
+// const adminStage = new Stage([setAdminWizard])
+// setAdminWizard.enter(async ctx => {
+//   await ctx.reply('ID?')
+// })
+
+// adminStage.hears('Вийти', async ctx => {
+//   await ctx.reply('Оберіть послугу в Меню', remove_kb)
+//   return ctx.scene.leave()
+// })
+// bot.use(adminStage.middleware())
+
+const stage = new Stage([gsWizard, setAdminWizard])
+// User services stage
+gsWizard.enter(async ctx => {
+  await ctx.reply('Будь-ласка, вкажіть ПІБ людини')
+})
+
+stage.hears('Вийти', async ctx => {
+  if (currentUser.role == 'User') {
+    await ctx.reply('Оберіть послугу в Меню', remove_kb)
+    return ctx.scene.leave()
+  } else if (currentUser.role == ('Admin' || 'Super_Admin')) {
+    await ctx.reply('Оберіть команду в меню', remove_kb)
+    return ctx.scene.leave()
+  }
+})
+
+stage.hears('Підтвердити', async ctx => {
+  await ctx.reply(
+    'Дякую, Ваші дані прийняті. Скоро ми надішлемо фото для звірки',
+    remove_kb,
+  )
+  return ctx.scene.leave()
+})
+//Admin services
+setAdminWizard.enter(async ctx => {
+  await ctx.reply('ID?')
+})
+
+bot.use(session(), stage.middleware())
 
 //BOT commands listeners
 //User's commands
 const userCommandsHandlers = () => {
   bot.command('grave_service', async ctx => {
     try {
-      await ctx.reply(
-        'Ви обрали послугу "Догляд за могилою". Далі буде дкілька питань, після чого ми надішлемо фото для попередньої звірки. Після підтверждення, ми доглянемо за могилою і надішлемо фото результату. Послуга коштує 39$.',
-      )
-      await ctx.scene.enter('graveServiceScene')
+      if (currentUser.role == 'User') {
+        await ctx.reply(
+          'Ви обрали послугу "Догляд за могилою". Далі буде дкілька питань, після чого ми надішлемо фото для попередньої звірки. Після підтверждення, ми доглянемо за могилою і надішлемо фото результату. Послуга коштує 39$.',
+        )
+        await ctx.scene.enter('graveServiceScene')
+      } else {
+        await findUser(ctx.chat.id)
+        await ctx.reply(
+          'Ви обрали послугу "Догляд за могилою". Далі буде дкілька питань, після чого ми надішлемо фото для попередньої звірки. Після підтверждення, ми доглянемо за могилою і надішлемо фото результату. Послуга коштує 39$.',
+        )
+        await ctx.scene.enter('graveServiceScene')
+      }
     } catch (err) {
       console.error(err)
     }
@@ -121,6 +132,7 @@ bot.action('accept_admin_invite', async ctx => {
   try {
     await ctx.answerCbQuery()
     console.log('Admin invite Accepted')
+    currentUser.role = ''
     base(tableUsers).update(
       [
         {
@@ -158,82 +170,87 @@ const findUser = async chatId => {
     chatId,
     `Ми перевіряємо Ваш обліковий запис...`,
   )
-  base(tableUsers)
-    .select({
-      filterByFormula: `{chat_id}=${chatId}`,
-      view: 'Grid view',
-      maxRecords: 1,
-    })
-    .firstPage(async function (err, records) {
-      console.log('Searching user in Airtable')
-      if (err) {
-        console.error(err)
-        return
-      }
-      if (records.length > 0) {
-        //console.log('Retrieved', record.get('chat_id'))
-        currentUser.role = records[0].get('role')
-        currentUser.airtable_id = records[0].getId()
-        console.log(currentUser)
-        if (currentUser.role === 'User') {
-          await bot.telegram.sendMessage(
-            chatId,
-            `Обліковий запис знайдено. Дякую, що зачекали`,
-          )
-          await bot.telegram.setMyCommands(userCommands)
-          await bot.telegram.sendMessage(
-            chatId,
-            `В Меню Ви можете обрати бажану послугу.`,
-          )
-        } else if (currentUser.role === ('Super_Admin' || 'Admin')) {
-          await bot.telegram.setMyCommands(adminCommands)
-          await bot.telegram.sendMessage(chatId, `Привіт Адмін`)
+  return new Promise((resolve, reject) => {
+    base(tableUsers)
+      .select({
+        filterByFormula: `{chat_id}=${chatId}`,
+        view: 'Grid view',
+        maxRecords: 1,
+      })
+      .firstPage(async function (err, records) {
+        console.log('Searching user in Airtable')
+        if (err) {
+          console.error('problem when retrieve data')
+          reject(err)
         }
-        console.log(records[0].getId())
-        return records[0].getId()
-      } else {
-        base(tableUsers).create(
-          [
-            {
-              fields: {
-                chat_id: chatId,
-                first_name: currentUser.first_name,
-                last_name: currentUser.last_name,
-                role: 'User',
+        if (records.length > 0) {
+          //console.log('Retrieved', record.get('chat_id'))
+          currentUser.role = records[0].get('role')
+          currentUser.airtable_id = records[0].getId()
+          console.log(currentUser)
+          if (currentUser.role === 'User') {
+            await bot.telegram.sendMessage(
+              chatId,
+              `Обліковий запис знайдено. Дякую, що зачекали`,
+            )
+            await bot.telegram.setMyCommands(userCommands)
+            await bot.telegram.sendMessage(
+              chatId,
+              `В Меню Ви можете обрати бажану послугу.`,
+            )
+            resolve(records[0].getId())
+          } else if (currentUser.role === ('Super_Admin' || 'Admin')) {
+            await bot.telegram.setMyCommands(adminCommands)
+            await bot.telegram.sendMessage(chatId, `Привіт Адмін`)
+          }
+          console.log(records[0].getId())
+          resolve(records[0].getId())
+        } else {
+          base(tableUsers).create(
+            [
+              {
+                fields: {
+                  chat_id: chatId,
+                  first_name: currentUser.first_name,
+                  last_name: currentUser.last_name,
+                  role: 'User',
+                },
               },
+            ],
+            async function (err, records) {
+              if (err) {
+                console.error('problem when retrieve data')
+                reject(err)
+              }
+              console.log('Creating user in Airtable')
+              //console.log('Retrieved', record.get('chat_id'))
+              currentUser.role = records[0].get('role')
+              currentUser.airtable_id = records[0].getId()
+              console.log(currentUser)
+              if (currentUser.role === 'User') {
+                await bot.telegram.setMyCommands(userCommands)
+                await bot.telegram.sendMessage(
+                  chatId,
+                  `Ваш обліковий запис створено.`,
+                )
+                resolve(records[0].getId())
+              } else if (currentUser.role === ('Super_Admin' || 'Admin')) {
+                await bot.telegram.setMyCommands(adminCommands)
+                await bot.telegram.sendMessage(chatId, `Привіт Адмін`)
+                resolve(records[0].getId())
+              }
             },
-          ],
-          async function (err, records) {
-            if (err) {
-              console.error(err)
-              return
-            }
-            console.log('Creating user in Airtable')
-            //console.log('Retrieved', record.get('chat_id'))
-            currentUser.role = records[0].get('role')
-            currentUser.airtable_id = records[0].getId()
-            console.log(currentUser)
-            if (currentUser.role === 'User') {
-              await bot.telegram.setMyCommands(userCommands)
-              await bot.telegram.sendMessage(
-                chatId,
-                `Ваш обліковий запис створено.`,
-              )
-            } else if (currentUser.role === ('Super_Admin' || 'Admin')) {
-              await bot.telegram.setMyCommands(adminCommands)
-              await bot.telegram.sendMessage(chatId, `Привіт Адмін`)
-            }
-          },
-        )
-        return
-      }
-    })
+          )
+        }
+      })
+  })
 }
 
 // Bot start
 const start = () => {
   bot.start(async ctx => {
     try {
+      ctx.scene.leave()
       await ctx.reply(
         `Моє шанування, ${ctx.message.from.first_name}. Вас вітає сервіс Сиреніті, ми допомагаємо зберегти спокій та безтурботність.`,
         remove_kb,
@@ -247,9 +264,9 @@ const start = () => {
       } else if (currentUser.role === ('Super_Admin' || 'Admin')) {
         await ctx.reply(`Привіт Адмін`)
       }
-      console.log(ctx.chat.id)
+      //console.log(ctx.chat.id)
     } catch (err) {
-      console.log(err)
+      console.error(err)
     }
   })
 }
@@ -258,7 +275,7 @@ start()
 if (process.env.NODE_ENV == 'development') {
   // if local use Long-polling
   bot.launch().then(() => {
-    console.info(`The bot ${bot.botInfo.username} is running locally`)
+    console.log(`The bot ${bot.botInfo.username} is running locally`)
   })
 } else {
   // Launch on webhook
@@ -270,7 +287,7 @@ if (process.env.NODE_ENV == 'development') {
       },
     })
     .then(() => {
-      console.info(`The bot ${bot.botInfo.username} is running on server`)
+      console.log(`The bot ${bot.botInfo.username} is running on server`)
     })
 }
 process.once('SIGINT', () => app.stop('SIGINT'))
